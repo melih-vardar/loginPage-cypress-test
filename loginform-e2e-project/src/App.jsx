d861beb34cbaa138896b9505053855cb1bb91a5c
Melih Vardar <mvardar97@gmail.com>
2024-10-04 05:52:31 +0300

    login-page-added

--- loginform-e2e-project/src/App.jsx
+++ loginform-e2e-project/src/App.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
 import './App.css'
+import Login from './components/Login'
+import { Switch, Route, useLocation } from 'react-router-dom';
+import Success from './components/Success';
 
-export default function App() {
+function App() {
+
   return (
-    <div>App</div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
+      <Route exact path="/Success">
+        <Success />
+      </Route>
+    </>
   )
 }
+
+export default App
--- loginform-e2e-project/src/components/Login.jsx
+++ loginform-e2e-project/src/components/Login.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Button,
+} from 'reactstrap';
+
+const initialForm = {
+    email: '',
+    password: '',
+    terms: false,
+};
 
 function Login() {
+
+
+    const [form, setForm] = useState(initialForm)
+
+    // HANDLE CHANGE ~~
+    const handleChange = (event) => {
+        let { name, value, type } = event.target;
+        value = type === 'checkbox' ? event.target.checked : value;
+        setForm({ ...form, [name]: value });
+        // console.log(`${name} : ${value}`)
+
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
-        <div>Login</div>
-    )
+        <Form onSubmit={handleSubmit} >
+            <FormGroup >
+                <Label for="exampleEmail" >Email</Label>
+                <Input
+                    id="exampleEmail"
+                    name="email"
+                    placeholder="Enter your email"
+                    type="email"
+                    onChange={handleChange}
+                    value={form.email}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="Enter your password "
+                    type="password"
+                    onChange={handleChange}
+                    value={form.password}
+                />
+            </FormGroup>
+            <FormGroup check>
+                <Input
+                    id="terms"
+                    name="terms"
+                    checked={form.terms}
+                    type="checkbox"
+                    onChange={handleChange}
+                />{' '}
+                <Label htmlFor="terms" check>
+                    I agree to terms of service and privacy policy
+                </Label>
+            </FormGroup>
+            <FormGroup >
+                <Button color="primary">
+                    Sign In
+                </Button>
+            </FormGroup>
+        </Form>
+    );
 }
 
 export default Login
