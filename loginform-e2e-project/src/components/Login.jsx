31951e494347245dd7ed7fb3dc7aff0a9e180f74
Melih Vardar <mvardar97@gmail.com>
2024-10-04 05:54:25 +0300

    form-validations-added

--- loginform-e2e-project/src/components/Login.jsx
+++ loginform-e2e-project/src/components/Login.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Form,
     FormGroup,
     Label,
     Input,
     Button,
+    FormFeedback,
 } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
 
 const initialForm = {
     email: '',
@@ -13,10 +15,39 @@ const initialForm = {
     terms: false,
 };
 
+const initialErrors = {
+    email: false,
+    password: false,
+    terms: false,
+}
+
+export const errorMessages = {
+    email: "Enter a valid E-mail",
+    password: "Your password must contain at least 8 characters, 1 uppercase letter, 1 lowercase letter, 1 special character and 1 number."
+}
+
 function Login() {
 
 
     const [form, setForm] = useState(initialForm)
+    const [isValid, setIsValid] = useState(false);
+    const [errors, setErrors] = useState(initialErrors)
+    const history = useHistory();
+
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+    const validatePassword = (password) => {
+        return String(password)
+            .match(
+                /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+            );
+    };
+
 
     // HANDLE CHANGE ~~
     const handleChange = (event) => {
@@ -25,14 +56,57 @@ function Login() {
         setForm({ ...form, [name]: value });
         // console.log(`${name} : ${value}`)
 
+        if (name === 'email') {
+            if (validateEmail(value)) {
+                setErrors({ ...errors, [name]: false });
+            } else {
+                setErrors({ ...errors, [name]: true });
+            }
+        }
+
+        if (name === 'password') {
+            if (validatePassword(value)) {
+                setErrors({ ...errors, [name]: false });
+            } else {
+                setErrors({ ...errors, [name]: true });
+            }
+        }
+
+        if (name === 'terms') {
+            if (value) {
+                setErrors({ ...errors, [name]: false });
+            } else {
+                setErrors({ ...errors, [name]: true });
+            }
+        }
     }
     const handleSubmit = (event) => {
         event.preventDefault();
+        if (!isValid) return;
+        setForm(initialForm);
+        history.push('/Success')
     }
-
+    useEffect(() => {
+        // console.log(form)
+        // console.log("email")
+        // console.log(validateEmail(form.email));
+        // console.log("password")
+        // console.log(validatePassword(form.password));
+        // console.log("terms")
+        // console.log(form.terms);
+        if (
+            validateEmail(form.email) &&
+            validatePassword(form.password) &&
+            form.terms
+        ) {
+            setIsValid(true);
+        } else {
+            setIsValid(false);
+        }
+    }, [form]);
     return (
-        <Form onSubmit={handleSubmit} >
-            <FormGroup >
+        <Form onSubmit={handleSubmit} data-cy="formGeneral">
+            <FormGroup data-cy="email">
                 <Label for="exampleEmail" >Email</Label>
                 <Input
                     id="exampleEmail"
@@ -41,7 +115,10 @@ function Login() {
                     type="email"
                     onChange={handleChange}
                     value={form.email}
+                    invalid={errors.email}
+                    data-cy="email-input"
                 />
+                {errors.email && <FormFeedback data-cy="error-message">{errorMessages.email}</FormFeedback>}
             </FormGroup>
             <FormGroup>
                 <Label for="examplePassword">Password</Label>
@@ -52,7 +129,10 @@ function Login() {
                     type="password"
                     onChange={handleChange}
                     value={form.password}
+                    invalid={errors.password}
+                    data-cy="password-input"
                 />
+                {errors.password && <FormFeedback data-cy="error-message">{errorMessages.password}</FormFeedback>}
             </FormGroup>
             <FormGroup check>
                 <Input
@@ -61,13 +141,15 @@ function Login() {
                     checked={form.terms}
                     type="checkbox"
                     onChange={handleChange}
+                    invalid={errors.terms}
+                    data-cy="checkbox-input"
                 />{' '}
                 <Label htmlFor="terms" check>
                     I agree to terms of service and privacy policy
                 </Label>
             </FormGroup>
             <FormGroup >
-                <Button color="primary">
+                <Button color="primary" disabled={!isValid} data-cy="submit-button">
                     Sign In
                 </Button>
             </FormGroup>
